8bd052ab161476300e1f27bb1373ea67a9f9875a
Zelayita <20240181@ricaldone.edu.sv>
2025-06-02 15:32:29 -0600

    Ahora yua se puede insertar 😁😁

--- js/script.js
+++ js/script.js
@@ -39,3 +39,63 @@ function CrearTabla(datos){ //"datos" representa al JSON que viene de la API
 }
 
 ObtenerPersonas();
+
+
+//Procesos para agreagar un nuevo registro.
+const modal = document.getElementById("modalAgregar"); //Cuadro de dialogo
+
+const btnAgregar = document.getElementById("btnAbrirModal"); // + para abrir
+
+const btnCerrar = document.getElementById("btnCerrarModal"); //X para cerrar 
+
+btnAgregar.addEventListener("click", () =>{
+    modal.showModal();
+});
+
+btnCerrar.addEventListener("click", () => {
+    modal.close();
+});
+
+//Agregar nuevo integrante desde el formulario
+document.getElementById("frmAgregar").addEventListener("submit", async  e => {
+    e.preventDefault(); // "e" repesenta el evento submit --- evita que el formulario se envie sin nada.
+
+    //Capturamos los valores del formulario
+    const nombre = document.getElementById("nombre").value.trim();
+    const apellido = document.getElementById("apellido").value.trim();
+    const edad = document.getElementById("edad").value.trim();
+    const Correo = document.getElementById("email").value.trim();
+
+    if(!nombre || !apellido || !edad || !Correo){
+        alert("Complete todos los campos");
+        return; //Evita que los campos vengan vacion y se siga ejecutando
+    }
+
+    //Llamar a la API para enviar el Ususario
+    const respuesta = await fetch(API_URL, {
+        method:"POST",
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({nombre, apellido, edad, Correo})
+    });
+
+    if(respuesta .ok){
+        alert("El Registro fue agregado recientemente")
+        //Limpiar el formulario
+        document.getElementById("frmAgregar").reset();
+        //Cerrar el formulario
+        modal.close();
+        //Recargar la tabla
+        ObtenerPersonas();
+    }
+    else{
+        alert("Hubo un eror al Agregar");
+    }
+
+});//Fin del formulario
+
+
+
+
+
+
+
